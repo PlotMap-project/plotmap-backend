cd66de2c12b64b1f2b7082900730489aeaef50ca
Sofa Gareeva <sofa@LAPTOP-J7LHIMLF.localdomain>
2026-04-22 19:25:11 +0300

    feat: make exceptions useful for android

---
+++ server/src/main/kotlin/com/plotmap/backend/config/CorsConfig.kt
@@ -0,0 +1,24 @@
+package com.plotmap.backend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val config = CorsConfiguration()
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+
+        return CorsFilter(source)
+    }
+}
